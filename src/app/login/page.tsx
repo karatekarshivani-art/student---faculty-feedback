1115a65a9378e15dddf74043d2195d3e3afd5da1
shivanib <karatekarshivani@gmail.com>
2026-05-02 19:51:48 +0530

    Fixed missing Link import in Login UI

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, Suspense } from 'react';
+import Link from 'next/link';
 import { useSearchParams, useRouter } from 'next/navigation';
 import styles from './login.module.css';
 
